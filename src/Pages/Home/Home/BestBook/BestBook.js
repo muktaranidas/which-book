313d0eab12e7f17f8b5e3470247e020ffbf73f74
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-27 00:46:10 -0800

    bestBook not complete

--- src/Pages/Home/Home/BestBook/BestBook.js
+++ src/Pages/Home/Home/BestBook/BestBook.js
@@ -5,6 +5,13 @@ const BestBook = () => {
   return (
     <section className="bg-gray-800 text-gray-100">
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+          <img
+            src="assets/svg/Business_SVG.svg"
+            alt=""
+            className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+          />
+        </div>
         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl">
             Ac mattis
@@ -31,13 +38,6 @@ const BestBook = () => {
               Malesuada
             </a>
           </div>
-        </div>
-        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-          <img
-            src="assets/svg/Business_SVG.svg"
-            alt=""
-            className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
-          />
         </div>
       </div>
     </section>
